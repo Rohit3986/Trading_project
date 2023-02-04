8c321c4d0f59f217fccbe2a5d1a0ca5f67454fd2
rohit3986 <rohitkashyap3986@gmail.com>
2023-02-04 10:16:23 +0530

    final commit

--- TradingProject/settings.py
+++ TradingProject/settings.py
@@ -119,9 +119,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 import os
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+# Base url to serve media files
+MEDIA_URL = '/media/'
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
