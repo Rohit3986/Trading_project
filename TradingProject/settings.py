040a51e82337efcc6fe7dfb8ec21eaffb2bd9753
rohit3986 <rohitkashyap3986@gmail.com>
2023-03-05 22:52:51 +0530

    debug setting

--- TradingProject/settings.py
+++ TradingProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-41hjo+-**obhfz&uk2u_@$oy!g139f3ekaa#b0oa1saeo#_ps#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
